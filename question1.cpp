77559d8eda0ef0f7d148cd626ff028b8093d4833
AngelF9 <112910128+AngelF9@users.noreply.github.com>
2023-05-23 13:10:01 -0700

    Create question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,18 @@
+// wario.cpp
+// Name: Angel Fuentes 
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  int y = 1; 
+  int z = 7;
+
+  x = y * z;
+  y += z; 
+
+  cout << "Hi!" << endl;
+  cout << "x = " << x << endl;   
+
+  return 0;
+}
